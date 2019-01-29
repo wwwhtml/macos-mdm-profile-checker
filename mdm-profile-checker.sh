a95e2f552f87f6d582f4561b6a2f8bde37c10d35
Daniel Arauz <danarauz@gmail.com>
2019-01-29 00:35:53 -0800

    making it look nicer and removed clear screen.

--- mdm-profile-checker.sh
+++ mdm-profile-checker.sh
@@ -6,42 +6,39 @@
 #Description: Works way faster if remote computer has the controller computer's SSH Public key already.
 #This script checks for MDM Profiles in remote MacOS computers:
 #Support files:
-#        ip-list.txt (add the computer IPs to check)
-#        MDM-Profile-Checker.sh
-#        file2.sh
+#	ip-list.txt (add the computer IPs to check)
+#	mdm-profile-checker.sh
+#	file2.sh
 #Steps:
-#        The script MDM-Profile-Checker.sh runs and grabs IPs from ip-list.txt.
+#        The script mdm-profile-checker.sh runs and grabs IPs from ip-list.txt.
 #        then checks if the IP responds to ping.
-#                If computer doesn't pings back:
+#                If computer doesn't replies back:
 #                        Records locally that the host is not online.
 #                        Then the scripts grabs the following IP from the ip-list.txt and the process starts again.
 #                If it pings back:
 #                        Via SCP sends the script file2.sh to the remote computer.
-#                        Then via SSH  runs file2.sh, it checks for Profiles.
+#                        Then via SSH runs file2.sh, it checks for MDM profiles.
 #        The results are logged locally.
 #
-# How to run it: sh MDM-Profile-Checker.sh ip-list.txt file2.sh
+# How to run it: sh mdm-profile-checker.sh ip-list.txt file2.sh
 # As today, 2019-01-27, in this script the remote admin account is 'radmin', replaced it with the one in your remote computer's to check. 
 # I intend to make a variable for that user, but not today. :)
 # +-------------------------------------------------------------------------------------------------------------------------------------+
 clear
-echo "MDM Profile Checker"
-echo "-------------------"
-echo "This script will connect to remote MacOS hosts and check if MDM profiles are installed or not."
+echo "+-------------------------------------------------------------------------------------+"
+echo "| This script checks for installed MDM Profiles on remote MacOS computers.            |"
+echo "+-------------------------------------------------------------------------------------+"
 echo " "
 echo "Files required for this task:"
-echo "    ip-list.txt"
-echo "    MDM-Profile-Checker.sh"
-echo "    file2.sh"
+echo "    ip-list.txt (list of IPs to scan)"
+echo "    mdm-profile-checker.sh (this one!)"
+echo "    file2.sh (script to be pushed to the remote machine)"
 echo " "
 echo "Results will be in: $(pwd)/"
 echo ""
 read -p "Press [ENTER] to continue, or CTRL+C to quit."
-clear
 echo ""
 echo "MDM Profile Checker in progress..."
-echo ""
-
 todaysdate=$(date +"%Y_%m_%d_%H_%M_%S")
 
 for ip in `cat $1`
